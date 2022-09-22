d4a80019fd37616daa412d1818f77749c18ba67d
Fabricio de Lima Costa <73128869+Fabricio-Lima@users.noreply.github.com>
2022-09-21 23:53:42 -0300

    adiciona metodos ao modulo usuario

--- src/app/pages/usuario/criar-usuario/criar-usuario.component.ts
+++ src/app/pages/usuario/criar-usuario/criar-usuario.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Usuario } from 'src/app/shared/models/usuario';
+import { UsuarioService } from '../../../shared/services/usuario.service';
 
 @Component({
   selector: 'app-criar-usuario',
@@ -8,20 +11,46 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class ComponenteCriarUsuario implements OnInit {
     form: FormGroup;
-    usuario: any;
+    usuario: Usuario;
 
-    constructor(private formBuilder: FormBuilder) { }
+    constructor(
+        private _formBuilder: FormBuilder,
+        private _activatedRouter: ActivatedRoute,
+        private _usuarioService: UsuarioService,
+        private _router: Router
+    ) {}
 
     ngOnInit(): void {
+        this.usuario = this._activatedRouter.snapshot.data.usuario != undefined ? this._activatedRouter.snapshot.data.usuario : {};
+
         this.initForm()
     }
 
     initForm() {
-        this.form = this.formBuilder.group({
+        this.form = this._formBuilder.group({
             nome: [this.usuario?.nome, [Validators.required, Validators.maxLength(30)]],
             tipo: [this.usuario?.tipo, [Validators.required]],
             senha: [this.usuario?.tipo, [Validators.required, Validators.minLength(5), Validators.maxLength(15)]],
             email: [this.usuario?.email, [Validators.required, Validators.pattern("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+).(\.[a-z]{2,3})$"), Validators.maxLength(50)]],
         });
+    }
+
+    salvarCadastro() {
+        if(!this.form.invalid) return;
+
+        const formData = {...this.form.value};
+
+        const usuarioData: Usuario = {
+            id: this.usuario?.id,
+            nome: formData.nome,
+            tipo: formData.tipo,
+            senha: formData.senha,
+            email: formData.email
+        }
+
+        return this._usuarioService.Salvar(usuarioData).subscribe({
+            next: () => this._router.navigate(["/usuario/listar"]),
+            error: (err: any) => console.log(err)
+        })
     }
 }
