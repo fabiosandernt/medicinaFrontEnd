71d1f89cea4f0c4c73cfb3cfb0f46cc9e76ab7d5
Fabricio de Lima Costa <73128869+Fabricio-Lima@users.noreply.github.com>
2022-09-28 00:06:09 -0300

    adiciona modals no compnente empresa

--- src/app/pages/empresa/criar-empresa/criar-empresa.component.ts
+++ src/app/pages/empresa/criar-empresa/criar-empresa.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit/modal';
+
+import { ComponenteModalCancel, ComponenteModalConfirm } from '../../../components/components.module';
+
 import { Empresa } from '../../../shared/models/empresa';
 import { EmpresaService } from '../../../shared/services/empresa.service';
-import { ModalComponent } from '../modal/modal.component';
 
 @Component({
     selector: 'app-criar-empresa',
@@ -17,18 +20,19 @@ export class ComponenteCriarEmpresa implements OnInit {
     form: FormGroup;
     empresa: Empresa;
 
+    modalRef: MdbModalRef<ComponenteModalCancel | ComponenteModalConfirm> | null = null;
+    //modalConfirmRef: MdbModalRef<ComponenteModalConfirm> | null = null;
+
+    _salvarCadastro: boolean = false;
+
     constructor(
-        private modalService: NgbModal,
         private formBuilder: FormBuilder,
         private router: Router,
         private activatedRouter: ActivatedRoute,
         private empresaService: EmpresaService,
         public dialog: MatDialog,
+        private modalMdbService: MdbModalService
     ) {}
-
-    // openModal(): void {
-    //     this.dialog.open(ComponenteModalMat, { width: '250px' });
-    // }
 
     ngOnInit(): void {
         this.empresa = this.activatedRouter.snapshot.data.empresa != undefined ? this.activatedRouter.snapshot.data.empresa : {};
@@ -36,7 +40,7 @@ export class ComponenteCriarEmpresa implements OnInit {
         this.initForm()
     }
 
-    initForm() {
+    initForm(): void {
         this.form = this.formBuilder.group({
             cnpj: [this.empresa?.cnpj, [Validators.required, Validators.minLength(14), Validators.maxLength(14)]],
             razaoSocial: [this.empresa?.razaoSocial, [Validators.required]],
@@ -44,47 +48,31 @@ export class ComponenteCriarEmpresa implements OnInit {
             telefone: [this.empresa?.telefone, [Validators.maxLength(9)]],
             risco: [this.empresa?.risco],
             endereco: [this.empresa?.endereco, [Validators.maxLength(50)]],
-            email: [this.empresa?.email, [Validators.required, Validators.pattern("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+).(\.[a-z]{2,3})$"), Validators.maxLength(50)]],
+            email: [this.empresa?.email,
+            [
+                Validators.required,
+                Validators.pattern("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+).(\.[a-z]{2,3})$"),
+                Validators.maxLength(50)
+            ]],
         });
     }
 
-    open(content: any) {
-        //this.modalService.open(content, { windowClass: 'modal-mini modal-primary', size: 'sm' })
-        this.modalService.open(content)
-        .result.then((result: any) =>
-        {
-            this.closeResult = `Closed with: ${result}`;
-        },
-        (reason: any) =>
-        {
-            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`
-        })
-    }
+    openModal(modalType: string): void {
+        if(!modalType) return;
 
-    openModal() {
-        this.modalService.open(ModalComponent,
-            { windowClass: 'modal-mini modal-primary', size: 'sm' }
-        ).result.then((result: any) =>
-        {
-            this.closeResult = `Closed with: ${result}`;
-        },
-        (reason: any) =>
-        {
-            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`
-        })
-    }
-
-    private getDismissReason(reason: any): string {
-        if (reason === ModalDismissReasons.ESC) {
-            return 'by pressing ESC';
-        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-            return 'by clicking on a backdrop';
-        } else {
-            return  `with: ${reason}`;
+        if(modalType === 'modalCancel'){
+            this.modalRef = this.modalMdbService.open(ComponenteModalCancel, { data: { routeReturn: "/empresa/listar" } })
+        }
+        else if(modalType === 'modalConfirm'){
+            this.modalRef = this.modalMdbService.open(ComponenteModalConfirm)
+            this.modalRef.onClose.subscribe((saveConfirm: any) => {
+                console.log(saveConfirm);
+                if(saveConfirm === true) this.salvarCadastro()
+            });
         }
     }
 
-    salvarCadastro() {
+    salvarCadastro(): any {
         if(!this.form.invalid) return;
 
         const formData = {...this.form.value};
